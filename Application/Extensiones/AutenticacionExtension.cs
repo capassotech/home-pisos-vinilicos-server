c17d01bd310c7306814bf882c611dd747774303d
Karen <karenceles10@gmail.com>
2024-09-13 00:44:56 -0300

    login funcionando

---
+++ Application/Extensiones/AutenticacionExtension.cs
@@ -0,0 +1,64 @@
+﻿using home_pisos_vinilicos.Application.DTOs;
+using home_pisos_vinilicos.Application.Services.ILogin;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace home_pisos_vinilicos.Application.Extensiones
+{
+    public class AutenticacionExtension : AuthenticationStateProvider
+    {
+        private readonly ISessionStorageService _sessionStorage;
+        private ClaimsPrincipal _sinInformacion = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public AutenticacionExtension(ISessionStorageService sessionStorage)
+        {
+            _sessionStorage = sessionStorage;
+        }
+
+        public async Task ActualizarEstadoAutenticacion(SesionDto? sesionUsuario)
+        {
+            ClaimsPrincipal claimsPrincipal;
+
+            if (sesionUsuario != null)
+            {
+                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
+                    new Claim(ClaimTypes.Email,sesionUsuario.Correo),
+                    new Claim(ClaimTypes.Role,sesionUsuario.Rol)
+                }, "JwtAuth"));
+
+                await _sessionStorage.GuardarStorage("sesionUsuario", sesionUsuario);
+            }
+            else
+            {
+                claimsPrincipal = _sinInformacion;
+                await _sessionStorage.RemoveItemAsync("sesionUsuario");
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+
+        }
+
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+
+            var sesionUsuario = await _sessionStorage.ObtenerStorage<SesionDto>("sesionUsuario");
+
+            if (sesionUsuario == null)
+                return await Task.FromResult(new AuthenticationState(_sinInformacion));
+
+            var claimPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
+                    new Claim(ClaimTypes.Email,sesionUsuario.Correo),
+                    new Claim(ClaimTypes.Role,sesionUsuario.Rol)
+                }, "JwtAuth"));
+
+
+            return await Task.FromResult(new AuthenticationState(claimPrincipal));
+
+        }
+    }
+}
